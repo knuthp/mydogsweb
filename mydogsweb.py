7b8ea454f3cc6a028c37a762e5297ec316175a56
Knut Helge Pedersen <p72@storebrand.no>
2016-08-08 15:27:44 +0200

    Added Stormpath

--- mydogsweb.py
+++ mydogsweb.py
@@ -1,9 +1,31 @@
 from flask import Flask
+from os import environ
+from flask_stormpath import StormpathManager
+from flask_login import login_required
 app = Flask(__name__)
+
+
+# Configure your app here.
+app.config['SECRET_KEY'] = environ['SECRET_KEY']
+app.config['STORMPATH_API_KEY_ID'] = environ['STORMPATH_API_KEY_ID']
+app.config['STORMPATH_API_KEY_SECRET'] = environ['STORMPATH_API_KEY_SECRET']
+app.config['STORMPATH_APPLICATION'] = 'mydogs'
+app.config['STORMPATH_ENABLE_REGISTRATION'] = False
+app.config['STORMPATH_ENABLE_FORGOT_PASSWORD'] = True
+
+stormpath_manager = StormpathManager(app)
+
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/secret')
+@login_required
+def secret():
+    return 'Super secret'
 
 
 
